ca70583fc28cdecef1edfd324fed412a8d1c55c5
matpato <matilde.pato@gmail.com>
2025-08-02 00:39:59 +0100

    Update website

--- src/components/profile/BriefBio.jsx
+++ src/components/profile/BriefBio.jsx
@@ -6,7 +6,7 @@ const BriefBio = () => {
     <div className="mb-2 bg-white dark:bg-gray-800 p-6 rounded-lg shadow-sm">
     <h2 className="text-2xl font-bold text-gray-900 dark:text-white mb-4">About Me</h2>
     <p className="text-gray-600 dark:text-gray-300">
-    Matilde Pato XPTO is an Adjunct Professor in Computer Science at the <a href="https://www.isel.pt/en/department/computer-engineering" target="_blank" rel="noopener noreferrer" className="font-bold">
+    Matilde Pato is an Adjunct Professor in Computer Science at the <a href="https://www.isel.pt/en/department/computer-engineering" target="_blank" rel="noopener noreferrer" className="font-bold">
     Department of Computer Engineering</a> of the <a href="https://www.isel.pt/en" target="_blank" rel="noopener noreferrer" className="font-bold">
     Lisbon School of Engineering</a> at <a href="https://www.ipl.pt/en/ureka" target="_blank" rel="noopener noreferrer" className="font-bold">Polytechnic University of Lisbon | Ureka</a>, 
     and a senior researcher at <span className="font-bold">IBEB</span> and <span className="font-bold">LASIGE</span> (FCUL) and <span className="font-bold">NOVA LINCS</span> (FCT-UNL). 
